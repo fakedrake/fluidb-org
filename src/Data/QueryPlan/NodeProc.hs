900ca0e7b7d5a77444d59dd9414cf856e049c137
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-04-29 02:39:32 +0100

    Added some minimal doc and a roadmap

--- src/Data/QueryPlan/NodeProc.hs
+++ src/Data/QueryPlan/NodeProc.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
@@ -5,12 +6,9 @@ module Data.QueryPlan.NodeProc
   (NodeProc) where
 
 import Control.Antisthenis.Convert
-import Data.Profunctor
 import Data.Utils.Monoid
 import Data.Utils.AShow
 import Control.Antisthenis.Zipper
-import Control.Antisthenis.Minimum
-import Control.Antisthenis.Sum
 import Control.Monad.State
 import Control.Monad.Writer hiding (Sum)
 import Control.Antisthenis.ATL.Transformers.Mealy
@@ -20,14 +18,18 @@ import Control.Arrow
 import Control.Monad.Reader
 import Data.NodeContainers
 
-newtype NodeProcMap n w m = NodeProcMap { unNodeProcMap :: RefMap n (ArrProc w m) }
+-- | A map containing all the proc maps. Mutually recursive with the
+-- proc itself.
+newtype NodeProcMap n w m =
+  NodeProcMap { unNodeProcMap :: RefMap n (ArrProc w m) }
 
--- for example ZEpoch v == RefMap n Version
+data EvalConf n = EvalConf { ecTrail :: NodeSet n }
+-- for example ZEpoch v == RefMap n IsMat
 type NodeProc n w = NodeProc0 n w w
 type NodeProc0 n w0 w =
   ArrProc
     w
-    (FixStateT (NodeProcMap n w0) (ReaderT (NodeSet n) (Writer (ZEpoch w0))))
+    (FixStateT (NodeProcMap n w0) (ReaderT (EvalConf n) (Writer (ZEpoch w0))))
 
 -- | Lookup a node process. If you can't find one substitute for the one
 -- provided.
@@ -41,19 +43,31 @@ luNodeProc (MealyArrow (Kleisli mkProc)) k = MealyArrow $ Kleisli go
         modify $ NodeProcMap . refInsert k nxt . unNodeProcMap
         return (MealyArrow $ Kleisli go,r)
 
-
+-- | depset has a constant part that is the cost of triggering and a
+-- variable part.
+data DSet n v = DSet { dsetConst :: Sum v,dsetNeigh :: [NodeProc n (Sum v)] }
 makeCostProc
   :: forall v n .
   (Num v,Ord v,AShow v,Monoid (ZEpoch (Sum v)))
-  => [[NodeProc n (Sum v)]]
+  => [DSet n v]
   -> NodeProc n (Sum v)
-makeCostProc neighbors =
-  convArrProc convMinSum
-  $ procMin
-  $ convArrProc convSumMin . procSum <$> neighbors
+makeCostProc deps = convArrProc convMinSum $ procMin $ go <$> deps
   where
+    go DSet {..} = convArrProc convSumMin $ procSum $ constArr : dsetNeigh
+      where
+        constArr = arr $ const $ BndRes dsetConst
     procMin :: [NodeProc0 n (Sum v) (Min v)]
             -> NodeProc0 n (Sum v) (Min v)
     procMin = mkProc
     procSum :: [NodeProc n (Sum v)] -> NodeProc n (Sum v)
     procSum = mkProc
+
+
+-- TODO: methods to
+--
+-- * Register the epoch.
+--
+-- * Check that the epoch meaningfully changed and return the old
+--   result.
+--
+-- *
