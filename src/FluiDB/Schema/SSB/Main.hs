03ba50736f5c06f16077874af9ecbc228bfbef99
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-02 19:19:00 +0000

    sync

--- src/FluiDB/Schema/SSB/Main.hs
+++ src/FluiDB/Schema/SSB/Main.hs
@@ -164,8 +164,8 @@ ssbMain :: IO ()
 ssbMain = do
   -- let oneGig = ResourceLimit 1000000000
   -- setResourceLimit ResourceDataSize (ResourceLimits oneGig oneGig)
-  let secs = 60
+  let secs = 60*5
   traceTM "Starting!"
-  timeout (secs * 1000000) (actualMain Verbose [1..6]) >>= \case
+  timeout (secs * 1000000) (actualMain Verbose [1..12]) >>= \case
     Nothing -> putStrLn $ printf  "TIMEOUT after %ds" secs
     Just () -> putStrLn "Done!"
