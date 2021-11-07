c6d76ca508a0e92d69b4d67b21090341aab2f2ab
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-07 16:12:49 +0000

    sync

--- branches.sh
+++ branches.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Building the benchmark"
-stack build -j 4 --ghc-options -DVERBOSE_SOLVING fluidb:bench:benchmark 2> /tmp/benchmark.out || true
+stack --work-dir .branches-stack-dir/ build -j 4 --ghc-options -DVERBOSE_SOLVING fluidb:bench:benchmark 2> /tmp/benchmark.out || true
 echo "Running readdump"
 rm -r /tmp/benchmark.out.bench_branches
 mkdir -p /tmp/benchmark.out.bench_branches
