62c914ee3f97867607c9b556ef650e0b876de56a
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-17 14:16:07 +0000

    sync

--- build-yaguajay.sh
+++ build-yaguajay.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
 # Push to git and build haskell on yaguajay then build c++ locally.
 
 git commit -a -m "sync"
 git push
-ssh christosp@yaguajay bash -c "cd /home/christosp/Projects/fluidb-org && git pull && ./build.sh"
+ssh christosp@yaguajay bash -c 'cd /home/christosp/Projects/fluidb-org && git pull && ./build.sh'
 # Copy the c++ files here
 scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload ./ssb-workload/
