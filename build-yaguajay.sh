c7e9f0a54918595bc98a711d03665edb568bf21d
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-17 23:36:21 +0000

    sync

--- build-yaguajay.sh
+++ build-yaguajay.sh
@@ -2,7 +2,7 @@
 set -ex
 # Push to git and build haskell on yaguajay then build c++ locally.
 
-git commit -a -m "sync"
+git commit -a -m "sync" || true
 git push
 ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && ./build.sh'"
 
