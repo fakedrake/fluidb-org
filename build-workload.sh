700f10b97edf422688b77048d725ae488146e61f
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-17 20:41:20 +0000

    sync

--- build-workload.sh
+++ build-workload.sh
@@ -12,7 +12,7 @@ set -xe
 
 cmake -S . -B ./cmake-build
 cmake --build ./cmake-build
-rm -r /tmp/fluidb-data/*
+rm -rf /tmp/fluidb-data/*
 for i in {1..27}; do
     echo "Running query ${i}"
     ./cmake-build/ssb-workload/query${i}
