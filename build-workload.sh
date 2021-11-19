c983d36420d3bc6b545bc382f20da27e96e3ec5b
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-19 16:13:03 +0000

    sync

--- build-workload.sh
+++ build-workload.sh
@@ -9,9 +9,14 @@ set -xe
 # done
 
 
+if  [[ ! -d ./cmake-build ]]; then
+    cmake -S . -B ./cmake-build
+else
+    echo "reusing ./cmake-build"
+fi
 
-cmake -S . -B ./cmake-build
-cmake --build ./cmake-build
+cmake --build ./cmake-build -j 4
+
 rm -rf /tmp/fluidb_store
 mkdir /tmp/fluidb_store
 for i in {1..27}; do
