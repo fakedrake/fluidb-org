d0b65b9867515229e52cddc1be47e30ef75cf27e
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-19 14:59:31 +0000

    sync

--- bama/include/record_map.hh
+++ bama/include/record_map.hh
@@ -313,14 +313,15 @@ void sortFile(const std::string& file) {
     return;
   }
   fmt::print("Not sorted... {}\n", file);
-  // std::sort(fs.begin(), fs.end(), cmp);
-  bubble_sort(fs.begin(), fs.end(), cmp);
+  std::sort(fs.begin(), fs.end(), cmp);
+  // bubble_sort(fs.begin(), fs.end(), cmp);
   // hsort<Page<typename Extract::Domain0>::allocation, Extract>(fs.begin(),
   //                                                             fs.end());
 }
 template<typename It,typename Cmp>
 void bubble_sort(It begin, It end, Cmp cmp) {
    for(It i = begin; i != end; i++) {
+     if (i - begin % 1000 == 0) fmt::print("Patience...\n");
       bool swaps = false;
       for(It it = begin; it != end - 1; it++) {
          if(not cmp(*it,*(it+1))) {
