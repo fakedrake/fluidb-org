5e6dd29e19c165b57dabd4d321f1a7a97ef7980b
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-20 22:58:39 +0000

    sync

--- src/Data/QueryPlan/Solve.hs
+++ src/Data/QueryPlan/Solve.hs
@@ -469,8 +469,9 @@ isDeletable ref = do
   isd <- getNodeState ref >>= \case
     Concrete _ Mat -> return False
     st              -> do
-      r0 <- withNoMat ref $ Mat.isMaterializable [] ref
-      -- r1 <- Mat.isMaterializable [] ref
+      -- r0 <- withNoMat ref $ Mat.isMaterializable [] ref
+      delDepMatCache ref
+      r0 <- Mat.isMaterializable [ref] ref
       -- when (r0 /= r1) $ throwPlan $ "Deletion is ambiguous for " ++ show ref
       st' <- getNodeState ref
       when (st /= st') $ throwPlan $ "Fucked the node state: " ++ show (ref,st,st')
