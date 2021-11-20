218b82ce48a09717e66fb7f1e4ddc87936d97085
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-20 21:18:17 +0000

    sync

--- build-yaguajay.sh
+++ build-yaguajay.sh
@@ -6,10 +6,10 @@ git commit -a -m "sync" || true
 git push
 ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && ./build.sh'"
 
+rm -rf ./ssb-workload/main/*
+rm -rf ./ssb-workload/indiv/*
 mkdir -p ./ssb-workload/main/
 mkdir -p ./ssb-workload/indiv/
-rm -r ./ssb-workload/main/query*
-rm -r ./ssb-workload/indiv/query*
 scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-main/* ./ssb-workload/main/
 scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-indiv/* ./ssb-workload/idiv/
 mkdir -p /tmp/fluidb-data/
