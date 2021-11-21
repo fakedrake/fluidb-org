73804f6e625155a484691ac30d36148cef05ce95
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-21 02:33:33 +0000

    sync

--- src/Data/QueryPlan/Matable.hs
+++ src/Data/QueryPlan/Matable.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE TypeApplications #-}
 module Data.QueryPlan.Matable
-  (isMaterializable) where
+  (isMaterializable,isMaterializableSlow) where
 import           Control.Antisthenis.Bool
 import           Control.Antisthenis.Types
+import           Control.Monad.Extra
 import           Data.NodeContainers
 import           Data.Proxy
 import           Data.QueryPlan.AntisthenisTypes
+import           Data.QueryPlan.MetaOp
 import           Data.QueryPlan.NodeProc
+import           Data.QueryPlan.Nodes
 import           Data.QueryPlan.Types
+import           Data.Utils.Functors
 
 isMaterializable
   :: forall t n m . Monad m => [NodeRef n] -> NodeRef n -> PlanT t n m Bool
@@ -22,3 +26,18 @@ isMaterializable noMats ref = do
     BndErr _e -> throwPlan "Antisthenis should have figured errors are False."
     BndRes (BoolV r) -> return r
     BndBnd _b -> throwPlan "Tried to force result but got bound"
+
+
+isMaterializableSlow
+  :: forall t n m . Monad m => [NodeRef n] -> NodeRef n -> PlanT t n m Bool
+isMaterializableSlow dels = go
+  where
+    go ref = if ref `elem` dels then return False else go0 ref
+    go0 ref = do
+      ism <- isMat <$> getNodeState ref
+      if ism then return True else checkMats ref
+    checkMats ref = do
+      neigh :: [[NodeRef n]] <- fmap2 (toNodeList . metaOpIn . fst)
+        $ findCostedMetaOps ref
+      anyM allMat neigh
+    allMat refs = allM go refs
--- src/Data/QueryPlan/Solve.hs
+++ src/Data/QueryPlan/Solve.hs
@@ -472,7 +472,7 @@ isDeletable ref = do
     Concrete _ Mat -> return False
     st              -> do
       -- r0 <- withNoMat ref $ Mat.isMaterializable [] ref
-      r0 <- Mat.isMaterializable [ref] ref
+      r0 <- Mat.isMaterializableSlow [ref] ref
       -- when (r0 /= r1) $ throwPlan $ "Deletion is ambiguous for " ++ show ref
       -- st' <- getNodeState ref
       -- when (st /= st') $ throwPlan $ "Fucked the node state: " ++ show (ref,st,st')
