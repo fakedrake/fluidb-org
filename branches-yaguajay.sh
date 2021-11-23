488e98889708e3c2b6161b06f3bc8536b5dd1fe4
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-23 23:12:19 +0000

    sync

--- branches-yaguajay.sh
+++ branches-yaguajay.sh
@@ -4,7 +4,7 @@ set -ex
 
 git commit -a -m "sync" || true
 git push
-ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && rm -f /tmp/branches.zip && ./branches.sh && cd /tmp zip -r branches.zip benchmark.out.bench_branches'"
+ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && rm -f /tmp/branches.zip && ./branches.sh && cd /tmp && zip -r branches.zip benchmark.out.bench_branches'"
 rm -rf /tmp/benchmark.out.bench_branches /tmp/branches.zip
 scp -r christosp@yaguajay:/tmp/branches.zip /tmp/
 cd /tmp/
