29d78bfeac14a70148e5da1aae815df9552189b5
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-24 01:15:25 +0000

    sync

--- build-yaguajay.sh
+++ build-yaguajay.sh
@@ -6,16 +6,16 @@ git commit -a -m "sync" || true
 git push
 ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && ./build.sh'"
 
-# rm -rf ./ssb-workload/main/*
-# rm -rf ./ssb-workload/indiv/*
-# mkdir -p ./ssb-workload/main/
-# mkdir -p ./ssb-workload/indiv/
-# scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-main/* ./ssb-workload/main/
-# scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-indiv/* ./ssb-workload/indiv/
-# mkdir -p /tmp/fluidb-data/
-# scp -r christosp@yaguajay:/run/user/1000/fluidb-data/*.dat /tmp/fluidb-data/
+rm -rf ./ssb-workload/main/*
+rm -rf ./ssb-workload/indiv/*
+mkdir -p ./ssb-workload/main/
+mkdir -p ./ssb-workload/indiv/
+scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-main/* ./ssb-workload/main/
+scp -r christosp@yaguajay:/run/user/1000/fluidb-data/workload-indiv/* ./ssb-workload/indiv/
+mkdir -p /tmp/fluidb-data/
+scp -r christosp@yaguajay:/run/user/1000/fluidb-data/*.dat /tmp/fluidb-data/
 
-# find ssb-workload/ \
-#      -name '*.cpp' \
-#      -exec sed -i '' -e 's|/run/user/1000/|/tmp/|g' \
-#                      {} \;
+find ssb-workload/ \
+     -name '*.cpp' \
+     -exec sed -i '' -e 's|/run/user/1000/|/tmp/|g' \
+                     {} \;
