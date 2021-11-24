b7b03f5083b736752d84eca5b77e3e0c34b5965b
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-11-24 01:26:55 +0000

    sync

--- src/Data/Codegen/Build.hs
+++ src/Data/Codegen/Build.hs
@@ -190,7 +190,7 @@ forEachEpoch pl = do
 
 
 cppReportPerf :: [CC.Statement CC.CodeSymbol]
-cppReportPerf = [CC.ExpressionSt $ CC.FunctionAp "report_counters" [] []]
+cppReportPerf = error "oops"  -- [CC.ExpressionSt $ CC.FunctionAp "report_counters" [] []]
 
 -- | The main function.
 getCppMain
