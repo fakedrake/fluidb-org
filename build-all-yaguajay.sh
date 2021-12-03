ac11c061bf87c44b8766ee399254372838b01d69
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-12-03 19:07:19 +0000

    sync

--- build-all-yaguajay.sh
+++ build-all-yaguajay.sh
@@ -4,4 +4,4 @@ set -ex
 
 git commit -a -m "sync" || true
 git push
-ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && rm -f /tmp/branches.zip && ./branches.sh && cd /tmp && zip -r branches.zip benchmark.out.bench_branches'"
+ssh christosp@yaguajay bash -c "'cd /home/christosp/Projects/fluidb-org && git pull && ./build-all.sh'"
