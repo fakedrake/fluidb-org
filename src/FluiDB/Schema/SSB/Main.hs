72e3201e0ae5b961dc8b73bad04082a0e8179159
Chris Perivolaropoulos <c.perivol@ed.ac.uk>
2021-12-24 19:08:03 +0000

    sync

--- src/FluiDB/Schema/SSB/Main.hs
+++ src/FluiDB/Schema/SSB/Main.hs
@@ -222,10 +222,10 @@ readGraph gpath m = do
 #endif
 
 workload :: [(WIndex, QueryId)]
-workload = take 30 $ zip [1 ..] $ cycle [1 .. 12]
+workload = take 30 $ zip [1 ..] $ cycle [1 .. 13]
 
 workload1 :: [(WIndex, QueryId)]
-workload1 = zip [1 ..] [1 .. 12]
+workload1 = zip [1 ..] [1 .. 13]
 
 ssbMainWorkload :: IO ()
 ssbMainWorkload = do
--- src/FluiDB/Schema/SSB/Queries.hs
+++ src/FluiDB/Schema/SSB/Queries.hs
@@ -214,12 +214,14 @@ ssbQueriesList :: [Query ExpTypeSym Table]
 ssbQueriesList =
   fmap
     (ssbParse . unwords)
-    [["select *"
-     ,"from lineorder, date"
-     ,"where lo_orderdate = d_datekey"
-     ,"and d_yearmonthnum = 199401"
-     ,"and lo_discount between 4 and 6"
-     ,"and lo_quantity between 26 and 35"]
+    [
+      -- Just a test
+      ["select *"
+      ,"from lineorder, date"
+      ,"where lo_orderdate = d_datekey"
+      ,"and d_yearmonthnum = 199401"
+      ,"and lo_discount between 4 and 6"
+      ,"and lo_quantity between 26 and 35"]
 
 
       -- Flight 1
